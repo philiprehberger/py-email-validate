bb850b84c3dd3c4107b6a0b22f64058877eb4318
Philip Rehberger <me@philiprehberger.com>
2021-10-30 18:57:30 -0600

    Release v0.2.0

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 [![Tests](https://github.com/philiprehberger/py-email-validate/actions/workflows/publish.yml/badge.svg)](https://github.com/philiprehberger/py-email-validate/actions/workflows/publish.yml)
 [![PyPI version](https://img.shields.io/pypi/v/philiprehberger-email-validate.svg)](https://pypi.org/project/philiprehberger-email-validate/)
+[![GitHub release](https://img.shields.io/github/v/release/philiprehberger/py-email-validate)](https://github.com/philiprehberger/py-email-validate/releases)
+[![Last updated](https://img.shields.io/github/last-commit/philiprehberger/py-email-validate)](https://github.com/philiprehberger/py-email-validate/commits/main)
 [![License](https://img.shields.io/github/license/philiprehberger/py-email-validate)](LICENSE)
+[![Bug Reports](https://img.shields.io/github/issues/philiprehberger/py-email-validate/bug)](https://github.com/philiprehberger/py-email-validate/issues?q=is%3Aissue+is%3Aopen+label%3Abug)
+[![Feature Requests](https://img.shields.io/github/issues/philiprehberger/py-email-validate/enhancement)](https://github.com/philiprehberger/py-email-validate/issues?q=is%3Aissue+is%3Aopen+label%3Aenhancement)
 [![Sponsor](https://img.shields.io/badge/sponsor-GitHub%20Sponsors-ec6cb9)](https://github.com/sponsors/philiprehberger)
 
 Email validation with syntax checking and normalization.
@@ -51,14 +55,38 @@ if not result.valid:
     print(result.error)  # "MX lookup failed for domain: example.com"
 ```
 
+### Bulk Validation
+
+```python
+from philiprehberger_email_validate import validate_many
+
+results = validate_many(["user@example.com", "bad@@email", "test@gmail.com"])
+for r in results:
+    print(r.normalized, r.valid)
+```
+
+### Disposable Email Detection
+
+```python
+from philiprehberger_email_validate import validate_email
+
+result = validate_email("user@mailinator.com")
+print(result.is_disposable)  # True
+
+result = validate_email("user@gmail.com")
+print(result.is_disposable)  # False
+```
+
 ## API
 
 | Function / Class | Description |
 |------------------|-------------|
-| `EmailResult` | Dataclass with `valid`, `normalized`, `domain`, and `error` fields |
+| `EmailResult` | Dataclass with `valid`, `normalized`, `domain`, `error`, and `is_disposable` fields |
 | `normalize(email)` | Strip whitespace and lowercase an email address |
 | `is_valid(email)` | Quick boolean syntax check |
 | `validate_email(email, check_mx=False)` | Full validation returning an `EmailResult` |
+| `validate_many(emails, check_mx=False)` | Validate multiple emails with optional parallel MX lookups |
+| `DISPOSABLE_DOMAINS` | Set of ~50 known disposable email domains |
 
 ## Development
 
@@ -67,6 +95,13 @@ pip install -e .
 python -m pytest tests/ -v
 ```
 
+## Support
+
+If you find this package useful, consider starring the repository.
+
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-Philip%20Rehberger-blue?logo=linkedin)](https://www.linkedin.com/in/philiprehberger/)
+[![More packages](https://img.shields.io/badge/More%20packages-philiprehberger-orange)](https://github.com/philiprehberger?tab=repositories)
+
 ## License
 
-MIT
+[MIT](LICENSE)
--- src/philiprehberger_email_validate/__init__.py
+++ src/philiprehberger_email_validate/__init__.py
@@ -2,15 +2,78 @@
 
 from __future__ import annotations
 
+import concurrent.futures
 import re
 import socket
 from dataclasses import dataclass, field
 
-__all__ = ["EmailResult", "normalize", "is_valid", "validate_email"]
+__all__ = [
+    "EmailResult",
+    "normalize",
+    "is_valid",
+    "validate_email",
+    "validate_many",
+]
 
 _EMAIL_REGEX = re.compile(
     r"^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$"
 )
+
+DISPOSABLE_DOMAINS: set[str] = {
+    "mailinator.com",
+    "guerrillamail.com",
+    "tempmail.com",
+    "throwaway.email",
+    "yopmail.com",
+    "sharklasers.com",
+    "grr.la",
+    "guerrillamailblock.com",
+    "pokemail.net",
+    "spam4.me",
+    "dispostable.com",
+    "maildrop.cc",
+    "discard.email",
+    "mailnesia.com",
+    "tempail.com",
+    "tempr.email",
+    "temp-mail.org",
+    "fakeinbox.com",
+    "mintemail.com",
+    "trashmail.com",
+    "trashmail.me",
+    "trashmail.net",
+    "mailcatch.com",
+    "mytemp.email",
+    "mohmal.com",
+    "burnermail.io",
+    "guerrillamail.info",
+    "guerrillamail.net",
+    "guerrillamail.org",
+    "guerrillamail.de",
+    "harakirimail.com",
+    "mailexpire.com",
+    "mailforspam.com",
+    "safetymail.info",
+    "binkmail.com",
+    "spamgourmet.com",
+    "getairmail.com",
+    "filzmail.com",
+    "inboxalias.com",
+    "jetable.org",
+    "mailnull.com",
+    "cuvox.de",
+    "armyspy.com",
+    "dayrep.com",
+    "einrot.com",
+    "fleckens.hu",
+    "gustr.com",
+    "jourrapide.com",
+    "rhyta.com",
+    "superrito.com",
+    "teleworm.us",
+    "tempinbox.com",
+    "trash-mail.com",
+}
 
 
 @dataclass
@@ -21,6 +84,7 @@ class EmailResult:
     normalized: str
     domain: str
     error: str = field(default="")
+    is_disposable: bool = field(default=False)
 
 
 def normalize(email: str) -> str:
@@ -54,6 +118,7 @@ def validate_email(email: str, *, check_mx: bool = False) -> EmailResult:
         )
 
     domain = normalized.rsplit("@", 1)[1]
+    disposable = domain in DISPOSABLE_DOMAINS
 
     if check_mx:
         try:
@@ -69,6 +134,36 @@ def validate_email(email: str, *, check_mx: bool = False) -> EmailResult:
                 normalized=normalized,
                 domain=domain,
                 error=f"MX lookup failed for domain: {domain}",
+                is_disposable=disposable,
             )
 
-    return EmailResult(valid=True, normalized=normalized, domain=domain)
+    return EmailResult(
+        valid=True,
+        normalized=normalized,
+        domain=domain,
+        is_disposable=disposable,
+    )
+
+
+def validate_many(
+    emails: list[str], *, check_mx: bool = False
+) -> list[EmailResult]:
+    """Validate multiple email addresses.
+
+    Args:
+        emails: List of email addresses to validate.
+        check_mx: If True, perform parallel MX lookups using a thread pool.
+
+    Returns:
+        A list of EmailResult objects, one per input email.
+    """
+    if check_mx:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results = list(
+                executor.map(
+                    lambda e: validate_email(e, check_mx=True), emails
+                )
+            )
+        return results
+
+    return [validate_email(email, check_mx=False) for email in emails]
